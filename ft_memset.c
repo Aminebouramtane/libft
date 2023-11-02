a9f7e2c9b4cc8f1ea890298e14f1833a9c1c73e0
Amine Bouramtane <abouramt@e1r1p3.1337.ma>
2023-11-02 21:57:47 +0100

    add ft_memset function

--- ft_memset.c
+++ ft_memset.c
@@ -2,7 +2,16 @@
 
 #include "libft.h"
 
-void *	ft_memset(void *b, int c, size_t len)
+void *ft_memset(void *b, int c, size_t len)
 {
+    char *ptr = b;
 
+    while (len--)
+    {
+        ptr = c;
+        ptr++;
+    }
+    return b;
 }
+
+// 00000100 00000100 00000100 00000100 
