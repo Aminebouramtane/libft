e69444ebf4a86bee5bd1a68b1d26146494c5e764
Amine Bouramtane <abouramt@e2r7p3.1337.ma>
2023-11-03 15:59:07 +0100

    ggg

---
+++ ft_memmove.c
@@ -0,0 +1,8 @@
+
+
+#include "libft.h"
+
+void    *ft_memmove(void *dst, const void *src, size_t len)
+{
+    
+}
--- libft.h
+++ libft.h
@@ -20,3 +20,4 @@ int ft_atoi(const char *str);
 void    *ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
