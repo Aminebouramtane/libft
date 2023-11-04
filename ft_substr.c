76052df8a2bcb7109da6c7276fffbf5097489668
Amine Bouramtane <abouramt@e2r8p4.1337.ma>
2023-11-04 14:51:21 +0100

    add ft_substr function

---
+++ ft_substr.c
@@ -0,0 +1,30 @@
+
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start,size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+
+
+	ptr = (char *)malloc(sizeof(char)*(len + 1));
+	if (ptr == 0)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			ptr[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
+
+// hello world, 6, 6
